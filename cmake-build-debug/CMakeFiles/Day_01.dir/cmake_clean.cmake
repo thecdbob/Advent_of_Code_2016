eb26e776f3225297747c2f89d8b1d8f55db1f439
cdbob <thecdbob@gmail.com>
2021-08-29 16:08:26 -0700

    Add a new project to Git

---
+++ cmake-build-debug/CMakeFiles/Day_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Day_01.dir/main.cpp.o"
+  "Day_01"
+  "Day_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Day_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+
+using namespace std;
+
+
+
+int part_1()
+{
+
+}
+
+int part_1()
+{
+
+}
+
+vector<int> file_input()
+{
+
+}
+
+int main()
+{
+
+    int input = file_input("input_day_01");
+    printf("Part One's Result is", part_1(file_input());
+    printf("Part Two's Result is", part_1(file_input());
+
+
+    return 0;
+}
